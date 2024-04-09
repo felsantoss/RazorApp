e61d1d5ac87fa921e09c21b49a6c64aa9c4f8d0a
Felipe Santos <felipesantossf@outlook.com>
2024-04-09 20:34:08 -0300

    Adicionando AddRazorPages indicando o uso de aplicação Razor Pages, UseHttpsRedirection para direcionar requisições HTTP para HTTPS, UseStaticFiles para utilizar arquivos estaticos na aplicação, UseRouting e MapRazorPages para mapear as rotas da aplicação

--- obj/Debug/net8.0/RazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000f7adc5d5b821412df341bcfce79d684accee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
